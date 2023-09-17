bb123f45911dc2d047352a7c0a800820c7640632
Divyanshu Gupta <98742408+divyanshu2712@users.noreply.github.com>
2023-09-17 18:25:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,39 +6,10 @@ from PIL import Image
 import tensorflow as tf
 import numpy as np
 import cv2
-
+#Some Globals
 image=None
 access=False
-Apple=tf.keras.models.load_model("models/Apple")
-Capsicum=tf.keras.models.load_model("models/Capsicum")
-Cherry=tf.keras.models.load_model("models/Cherry")
-Chilli=tf.keras.models.load_model("models/Chilli")
-Corn=tf.keras.models.load_model("models/Corn")
-Grape=tf.keras.models.load_model("models/Grape")
-Peach=tf.keras.models.load_model("models/Peach")
-Potato=tf.keras.models.load_model("models/Potato")
-Rice=tf.keras.models.load_model("models/Rice")
-Strawberry=tf.keras.models.load_model("models/Strawberry")
-Sugarcane=tf.keras.models.load_model("models/Sugarcane")
-Tea=tf.keras.models.load_model("models/Tea")
-Tomato=tf.keras.models.load_model("models/Tomato")
-Wheat=tf.keras.models.load_model("models/Wheat")
-models = {
-    "Apple": Apple,
-    "Capsicum": Capsicum,
-    "Cherry": Cherry,
-    "Chilli": Chilli,
-    "Corn": Corn,
-    "Grape": Grape,
-    "Peach": Peach,
-    "Potato": Potato,
-    "Rice": Rice,
-    "Strawberry": Strawberry,
-    "Sugarcane": Sugarcane,
-    "Tea": Tea,
-    "Tomato": Tomato,
-    "Wheat": Wheat
-}
+#App Configuration
 app = Flask(__name__)
 app.config.update(DROPZONE_TIMEOUT=6*5*1000)
 app.config['DROPZONE_UPLOAD_MULTIPLE'] = False
@@ -47,10 +18,10 @@ app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image/*'
 app.config['DROPZONE_MAX_FILE_EXCEED']=1
 app.config['DROPZONE_DEFAULT_MESSAGE']="Place Your Plant Image Here"
 app.config['DROPZONE_MAX_FILE_SIZE']=80
-
+#Defining Dropzone
 dropzone=Dropzone(app)
 
-
+#Prediction Page Working
 @app.route('/predict',methods=['GET','POST'])
 def predict():
     global image
@@ -62,17 +33,16 @@ def predict():
         image=np.expand_dims(image,axis=0)
         access=True
     return render_template("predict.html")
+#Result page working and predicting class
 @app.route('/result',methods=["POST"])
 def result():
     global image
     a=request.form.get("d_name")
-    print(a)
-    # model=tf.keras.models.load_model(f"models/{a}")
-    model=models[a]
+    model=tf.keras.models.load_model(f"models/{a}")
     pred=model.predict(image)
     class_pred=classes[a][np.argmax(pred)]
     confidence=round(np.max(pred)*100,2)
-    if confidence!=None:
+    if access:
         if(class_pred=="healthy"):
             healthy="Healthy"
             disease=False
@@ -85,15 +55,15 @@ def result():
             return render_template("result.html",healthy=healthy,disease=disease,disease_cure=disease_cure,disease_cure_links=disease_cure_links,class_pred=class_pred,confidence=confidence)
     else:
         abort(403)
-
+#About us page rendering
 @app.route("/about")
 def aboutus():
-    global confidence
-    confidence=None
+    global access
+    access=False
     return render_template("aboutus.html")
-
+#Home Page
 @app.route("/")
 def index():
-    global confidence
-    confidence=None
+    global access
+    access=False
     return render_template("index.html")
